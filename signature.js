1a34aac3633ed14d0d60b5f1ad2fa3493eb89b6b
Fábio Santos <fabiosantosart@gmail.com>
2013-05-17 14:09:12 +0100

    starting things up

---
+++ signature.js
@@ -0,0 +1,9 @@
+(function () {
+'use strict';
+module.exports = signature
+exports.reader = reader
+exports.configuration = {}
+
+function signature() {}
+function reader() {}
+}())
---
+++ test.js
@@ -0,0 +1,21 @@
+/*jshint asi:true*/
+
+var signature = require('./signature')
+var should = require('should')
+
+(function () {
+'use strict';
+
+describe('Argument reader', function () {
+    // TODO it('lists function arguments', function () {})
+    // TODO it('detects "rest", "asObject", and "arguments"', function () {})
+    // TODO it('can be configured', function () {})
+})
+
+describe('signature modifier', function () {
+    // TODO it('modifies signatures', function () {})
+    // TODO it('can be called with configuration options', function () {})
+    // TODO it('...which are respected by the argument reader', function () {})
+})
+
+}())
